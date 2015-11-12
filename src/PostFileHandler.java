daf3b2187a81973a03d133567b2d211978a5727b
jarvisoa <jarvisoa@rose-hulman.edu>
2015-11-12 01:03:35 -0500

    postfile

---
+++ src/PostFileHandler.java
@@ -0,0 +1,13 @@
+import protocol.HttpRequest;
+import protocol.HttpResponse;
+import protocol.IRequestHandler;
+
+public class PostFileHandler implements IRequestHandler{
+
+	@Override
+	public HttpResponse handleRequest(HttpRequest request, String rootDir) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
