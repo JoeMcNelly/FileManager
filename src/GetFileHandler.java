79f2a50df91817c564e9fe03b3988f6543a88e52
JoeMcNelly <joemcnelly1@gmail.com>
2015-11-12 01:16:49 -0500

    Orry test this

---
+++ src/GetFileHandler.java
@@ -0,0 +1,29 @@
+import java.io.File;
+
+import protocol.HttpRequest;
+import protocol.HttpResponse;
+import protocol.HttpResponseFactory;
+import protocol.IRequestHandler;
+import protocol.Protocol;
+
+public class GetFileHandler implements IRequestHandler {
+
+	@Override
+	public HttpResponse handleRequest(HttpRequest request, String rootDir) {
+		HttpResponse response = null;
+		
+		String[] uri = request.getUri().split("/");
+		String fileName = "";
+		if (uri.length == 3){
+			fileName = uri[2];
+		}
+		String resourcesDir = "./FileManagerResources/";
+		File file = new File(resourcesDir + fileName);
+		if (file.exists()){
+			response = HttpResponseFactory.create200OK(file, Protocol.CLOSE);
+		}else{
+			response = HttpResponseFactory.create404NotFound(Protocol.CLOSE);
+		}
+		return response;
+	}
+}
