84396a6c05e8d790cb8586af9825b670b6e915dd
jarvisoa <jarvisoa@rose-hulman.edu>
2015-11-11 23:42:55 -0500

    here

--- src/FileManager.java
+++ src/FileManager.java
@@ -1,4 +1,13 @@
+import Plugin.IPlugin;
+import protocol.HttpRequest;
+import protocol.HttpResponse;
 
-public class FileManager {
+public class FileManager implements IPlugin{
+
+	@Override
+	public HttpResponse handle(HttpRequest request, String rootDir) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
