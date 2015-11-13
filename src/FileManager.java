1b6a1112e4e751d48c3e0fb8a015c57728ac6889
Jordan LaShomb <jtlashomb9@gmail.com>
2015-11-12 21:30:30 -0500

    commit

--- src/FileManager.java
+++ src/FileManager.java
@@ -1,44 +1,44 @@
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-import Plugin.IPlugin;
-import Servlet.IServlet;
 import protocol.HttpRequest;
 import protocol.HttpResponse;
 import protocol.HttpResponseFactory;
 import protocol.Protocol;
+import Plugin.IPlugin;
+import Servlet.IServlet;
 
 public class FileManager implements IPlugin {
 
 	private Map<String, IServlet> servlets;
 	private static FileManager instance;
 	private File baseHtml;
-	private static final String DIR_PATH = "/web";
-	private static final String PATH = "/FileManager.html";
+	private static final String WORK_DIR = System.getProperty("user.dir");
+	private static final String DIR_PATH = System.getProperty("file.separator")
+			+ "FileManagerResources";
+	private static final String PATH = System.getProperty("file.separator")
+			+ "FileManager.html";
 
 	public FileManager() {
 		servlets = new HashMap<String, IServlet>();
 		servlets.put("getAllFiles", new GetAllFilesServlet());
 		servlets.put("file", new GetFileServlet());
-		
+
 		File dir = new File(DIR_PATH);
-		if (dir.exists()||!dir.isDirectory()){
+		if (!dir.exists() || !dir.isDirectory()) {
 			dir.delete();
 			dir.mkdir();
 		}
-		baseHtml = new File(DIR_PATH+PATH);
-		if(baseHtml.exists()){
+		baseHtml = new File(WORK_DIR + DIR_PATH + PATH);
+		if (!baseHtml.exists()) {
 			baseHtml.delete();
 		}
 		try {
 			baseHtml.createNewFile();
-			FileOutputStream writer = new FileOutputStream(baseHtml, true);
-			writer.close();
-			
 		} catch (IOException e) {
+			e.printStackTrace();
 		}
 		instance = this;
 	}
@@ -55,13 +55,12 @@ public class FileManager implements IPlugin {
 			return HttpResponseFactory.create400BadRequest(Protocol.CLOSE);
 		}
 	}
-	
+
 	public static FileManager getInstance() {
 		return instance;
 	}
-	
+
 	public File getBaseHTML() {
-		System.out.println("baseHTML: "+baseHtml.getAbsolutePath());
 		return baseHtml;
 	}
 
